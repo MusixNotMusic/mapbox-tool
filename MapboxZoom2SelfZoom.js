2d01c5bb7d991d38ddf3369eb6ea6011d4e93aaf
o_O <musixnotmusic@gmail.com>
2024-05-28 14:07:30 +0800

    Update MapboxZoom2SelfZoom.js

--- MapboxZoom2SelfZoom.js
+++ MapboxZoom2SelfZoom.js
@@ -2,6 +2,101 @@
  * mapboxgl latitude 40 meters/pixel
  * https://docs.mapbox.com/help/glossary/zoom-level/
  */
+let zoomIn0LatMao = [
+    {
+        "zoom": 0,
+        "meters": 78271.484
+    },
+    {
+        "zoom": 1,
+        "meters": 39135.742
+    },
+    {
+        "zoom": 2,
+        "meters": 19567.871
+    },
+    {
+        "zoom": 3,
+        "meters": 9783.9355
+    },
+    {
+        "zoom": 4,
+        "meters": 4891.96775
+    },
+    {
+        "zoom": 5,
+        "meters": 2445.983875
+    },
+    {
+        "zoom": 6,
+        "meters": 1222.9919375
+    },
+    {
+        "zoom": 7,
+        "meters": 611.49596875
+    },
+    {
+        "zoom": 8,
+        "meters": 305.747984375
+    },
+    {
+        "zoom": 9,
+        "meters": 152.8739921875
+    },
+    {
+        "zoom": 10,
+        "meters": 76.43699609375
+    },
+    {
+        "zoom": 11,
+        "meters": 38.218498046875
+    },
+    {
+        "zoom": 12,
+        "meters": 19.1092490234375
+    },
+    {
+        "zoom": 13,
+        "meters": 9.55462451171875
+    },
+    {
+        "zoom": 14,
+        "meters": 4.777312255859375
+    },
+    {
+        "zoom": 15,
+        "meters": 2.3886561279296874
+    },
+    {
+        "zoom": 16,
+        "meters": 1.1943280639648437
+    },
+    {
+        "zoom": 17,
+        "meters": 0.5971640319824219
+    },
+    {
+        "zoom": 18,
+        "meters": 0.2985820159912109
+    },
+    {
+        "zoom": 19,
+        "meters": 0.14929100799560546
+    },
+    {
+        "zoom": 20,
+        "meters": 0.07464550399780273
+    },
+    {
+        "zoom": 21,
+        "meters": 0.037322751998901366
+    },
+    {
+        "zoom": 22,
+        "meters": 0.018661375999450683
+    }
+];
+
 const zoomIn40LatMap = [
     {
         "zoom": 0,
